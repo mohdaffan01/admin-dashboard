16573c28df6527709fdfe46533e91d2db3da6a21
affan <mohdaffan338@gmail.com>
2026-07-01 00:59:45 +0530

    add frontend url in cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ import { errorMiddleware } from "./middleware/error.middleware.js";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://admin-dashboard-hoxmbn5r0-mohdaffan01s-projects.vercel.app"
+  ],
   credentials: true
 }));
 app.use(express.json({ limit: "20mb" }));
